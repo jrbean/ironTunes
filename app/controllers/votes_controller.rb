7438ded5f957dec22076c4a679879a81e7883eaa
Justin Bean <beanjustinr@gmail.com>
2015-11-20 13:58:30 -0500

    votes controller work

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,15 @@
 class VotesController < ApplicationController
+
+  def create
+    @vote = Vote.new
+    @vote.user_id = current_user.id
+    @vote.song_id = params[:song_id].to_i
+
+    if v.vote_check_passed
+      v.save!
+    else
+      flash[:notice] = "Something seems to have gone wrong."
+    end
+    redirect_to :index
+  end
 end
